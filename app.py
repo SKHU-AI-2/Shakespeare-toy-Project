3aa6eafd9a7202d2962747780a76b2182f603f8f
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2024-02-28 01:18:33 +0900

    Bug Fix
    1.requirements.txt 버전 수정
    2.app.py 모델 수정

--- app.py
+++ app.py
@@ -4,16 +4,15 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from tensorflow.keras.models import load_model
 import numpy as np
-import os
 
 app = FastAPI()
 
-# 모델 및 데이터 초기화
+# 모델 로드
 model_path = 'shakespeare_text_gen_model.h5'
-text_path = 'shakespeare.txt'
 model = load_model(model_path)
-text = open(text_path, 'rb').read().decode('utf-8')
-chars = sorted(set(text))
+
+# 고유 문자 및 매핑 초기화
+chars = ['\n', ' ', '!', '"', "'", '(', ')', ',', '-', '.', ':', ';', '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 char_to_index = dict((c, i) for i, c in enumerate(chars))
 index_to_char = dict((i, c) for i, c in enumerate(chars))
 
@@ -23,19 +22,19 @@ templates = Jinja2Templates(directory="templates")
 
 # 텍스트 생성 함수
 def generate_custom_text(length, diversity):
-    start_index = np.random.randint(0, len(text) - 40 - 1)
+    start_index = np.random.randint(0, len(chars) - 1)
     generated = ''
-    sentence = text[start_index: start_index + 40]
-    generated += sentence
+    sentence = [np.random.choice(chars) for _ in range(40)]
+    generated += ''.join(sentence)
     for i in range(length):
-        x_pred = np.zeros((1, 40, len(chars)), dtype=np.bool_)
+        x_pred = np.zeros((1, 40, len(chars)))
         for t, char in enumerate(sentence):
             x_pred[0, t, char_to_index[char]] = 1.
         preds = model.predict(x_pred, verbose=0)[0]
         next_index = np.random.choice(len(chars), p=preds)
         next_char = index_to_char[next_index]
         generated += next_char
-        sentence = sentence[1:] + next_char
+        sentence = sentence[1:] + [next_char]
     return generated
 
 @app.get("/", response_class=HTMLResponse)
